5da78a9b24f89375fdb61c6847fbde9f24e3a197
Stelios Kamnakis <kamnis1997@gmail.com>
2021-07-13 13:35:56 +0300

    Styles

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ import axios from 'axios'
 function showComponent (data) {
   const dialog = `
     <div class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-      <div class="w-64 h-64 bg-white shadow">
+      <div class="w-96 h-64 bg-white shadow overflow-y-auto p-4">
         <h1>Hello World!</h1>
-        <pre class="break-all">${JSON.stringify(data)}</pre>
+        <h1 class="break-words">${JSON.stringify(data)}</h1>
       </div>
     </div>
   `
