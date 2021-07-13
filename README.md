2fdf65899615e2072c00cc3aa1501ffe4ead4ee0
Stelios Kamnakis <kamnis1997@gmail.com>
2021-07-13 12:33:14 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# This is a test package
+
+`npm i https://github.com/kamnakis/mypackage.git`
---
+++ index.js
@@ -0,0 +1,7 @@
+function mypackage () {
+  window.alert('This is an alert!')
+}
+
+module.exports = {
+  mypackage
+}
