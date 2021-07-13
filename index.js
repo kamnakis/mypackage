4a36f9a7c7e24f0e27ee3ad34e9d7d3f85c1b53c
Stelios Kamnakis <kamnis1997@gmail.com>
2021-07-13 13:18:42 +0300

    Test html

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 import axios from 'axios'
 
+function showComponent (data) {
+  const dialog = `
+  <div class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+    <div class="w-64 h-64 bg-white shadow">
+        <h1>Hello World!</h1>
+        <pre>${data}</pre>
+    </div>
+  </div>
+  `
+
+  document.body.insertAdjacentHTML('afterend', dialog)
+}
+
 export async function mypackage (options) {
-  const data = await axios.get(options.endpoint)
+  const { data } = await axios.get(options.endpoint)
 
   console.log(data)
+  showComponent(data)
 }
